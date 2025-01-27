06383a1a90b4bfd1782b842dd8e077d129b797b3
KaustavBhardwaj <kaustav1187.becse24@chitkara.edu.in>
2025-01-27 13:48:48 +0530

    Solution to Logical NOT and AND Combination

--- Logical NOT and AND Combination@@67882a731b4392d8fc1577e0/code.c
+++ Logical NOT and AND Combination@@67882a731b4392d8fc1577e0/code.c
@@ -3,10 +3,10 @@ int main(){
     int a ,b;
     printf("");
     scanf("%d %d",&a,&b);
-    if (a<=0  || b<=0){
+    if (a<0  || b<0){
         printf("True");
     }else{
-        printf("False");
+        printf("");
         }
     return 0;
 }
